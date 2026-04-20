816fa4b322cab84de26027c69153aa40b6b2c386
Unserved-sleep <128551791+Unserved-sleep@users.noreply.github.com>
2026-04-20 10:45:56 +0530

    Aditya - Day8 Programs- complete

--- Day8/regexx2.py
+++ Day8/regexx2.py
@@ -1,8 +1,21 @@
 import re
 
-text1 = "I have 2 apples and 5 oranges. 23 345 45.5 5.6  5,6"
-print(re.findall(r'\d+', text1))
+text1 = "I have 2 apples and 5 oranges. 23 345 45.5 5.6   5,6"
+print(re.findall(r'\d+', text1)) #digits
 print(re.findall(r'\d{2}', text1))
-print(re.findall(r'\D+', text1))
+print(re.findall(r'\D+', text1)) #non digits
 print(re.findall(r'\D{3}', text1))
-print(re.findall(r'\s+', text1))
+print(re.findall(r'\s+', text1)) #space
+print(re.findall(r'\s{2}', text1))
+print(re.findall(r'\S{2}', text1)) #non space
+print(re.findall(r'\w{3}', text1)) #word-alphanumeric
+print(re.findall(r'\W{2}', text1))
+print(re.findall("[a-z]+", text1)) #alphabets
+print(re.findall("[a-zA-z]+", text1))
+print(re.match(r"^I ha", text1)) #start with
+print(re.search(r"5,6$", text1)) #ends on
+print(re.sub(r"\d+", "X", text1)) #replaceing
+print(re.split(r"\s+", text1)) #split
+email = "abc123@gmail.com"
+print(re.match(r"^[\w.-]+@[\w.-]+\.\w+$", email)) #gmail
+print(re.findall(r"[\w.-]+@gmail\.\w+", email))
