2747f8f145ad669a47a5306e0061887757f71f82
Unserved-sleep <128551791+Unserved-sleep@users.noreply.github.com>
2026-06-08 11:48:10 +0530

    fastapi[Aditya]: queries

---
+++ main.py
@@ -0,0 +1,65 @@
+'''from enum import Enum
+
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10 ):
+# async def read_item(item_id: str, q: str | None = None):  default = none
+    return fake_items_db[skip : skip + limit]
+
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:  1 true True on yes
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item '''
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+#http://127.0.0.1:8000/items/foo-item?needy=sooooneedy
